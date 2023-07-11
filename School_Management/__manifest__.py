01b5c10319601c01dc1d79bf273df4eb78c0d98f
amandeep8297 <amandeep.singh@brainvire.com>
2023-07-11 17:40:56 +0530

    updated the module

--- School_Management/__manifest__.py
+++ School_Management/__manifest__.py
@@ -8,8 +8,8 @@
     'application': True,
     'data':[
         'security/ir.model.access.csv',
+        'wizard/new_admission_view.xml',
         'views/school_form.xml',
-        'views/demo.xml',
-        
+        'views/demo.xml', 
     ]
 }
